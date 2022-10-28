e28325be5aebcb6c0ef6cfa19a4be885d1c23bbd
Emmanuel Alafonye <ke.alafonye@yahoo.com>
2022-10-27 22:06:34 -0600

    question1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navigations from "./components/Navigations";
+import Home from "./pages/Home";
+import Universities from "./pages/Universities";
+// import Footer from "./components/Footer";
+import PostalLookup from "./pages/PostalLookup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigations />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/universities" element={<Universities />} />
+        <Route path="/postalLookup" element={<PostalLookup />} />
+      </Routes>
+      {/* <Footer /> */}
+    </>
   );
 }
 
